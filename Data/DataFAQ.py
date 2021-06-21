866052c552c5d6fbd68efbacecc1d8b0581e4cbf
WenTingTseng <d0409857@gmail.com>
2021-06-21 17:49:48 +0800

    change DataFAQ.py

--- Data/DataFAQ.py
+++ Data/DataFAQ.py
@@ -1,12 +1,22 @@
 ##################產生資料faq.txt################
 import sys
+import difflib
+def get_key (dict, value):
+    for k, v in dict.items():  
+        if v == value:
+            return [k]
 train_data_file='traindata.txt'
-answer_file='北衛_answer.txt',
+question_file='北衛_question.txt'
+answer_file='北衛_answer.txt'
 index_file='北衛_index.txt'
 output_file="faq.txt"
 
 fp = open(train_data_file, "r")
 train_data = fp.readlines()
+fp.close()
+
+fp = open(question_file, "r")
+questions = fp.readlines()
 fp.close()
 
 fp = open(answer_file, "r")
@@ -17,25 +27,21 @@ fp = open(index_file, "r")
 index = fp.readlines()
 fp.close()
 
+idxtoq=dict()
 idxtoa=dict()
-numtoidx=dict()
-
-
-for idx,a in zip(index,answers):
-    idxtoa[idx]=a
-
-index=set(index)
-for num,idx in enumerate(index):
-    numtoidx[num]=idx
+for i,q in enumerate(questions):
+    idxtoq[i]=q.strip('\n')
+for i,a in enumerate(answers):
+    idxtoa[i]=a.strip('\n')
 
 #寫檔
 wp = open(output_file, "w")
 k=0
 for data in train_data:
-    # wp.writelines('\n'+str(k)+'\n')
+    data=data.strip('\n')
     wp.writelines(data+'\n')
-    data=data.split()
-    wp.writelines("   "+idxtoa[numtoidx[int(data[0])]])
+    question=data.split()[1]
+    print(question)
+    wp.writelines("   "+idxtoa[get_key(idxtoq,question)[0]]+'\n')
     k+=1
-print(k)
 wp.close()
